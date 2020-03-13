aa821e74a2dc0bd142cb091caeb0bb1c9af416c6
adriennem76 <50639575+adriennem76@users.noreply.github.com>
2020-03-13 19:14:49 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 user1 = User.create(name: "Joe", email: "joe@gmail.com",  password: "1111" )
 user2 = User.create(name: "Anna", email: "anna@gmail.com",  password: "1111" )
 user3 = User.create(name: "Dylan", email: "dylan@gmail.com",  password: "1111" )
+user4 = User.create(name: "Alice", email: "Alice@gmail.com",  password: "1111" )
 
 supportItem1 = SupportItem.create(category: "practical", description: "car rides", frequency_num: 2, frequency_period: "week", user_id: 1)
 supportItem2 = SupportItem.create(category: "emotional support", description: "phone calls", frequency_num: 1, frequency_period: "week", user_id: 2)
